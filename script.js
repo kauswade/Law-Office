ca2a3ebc446c8e6513c396192ff4c841a9d847d5
kaustubh wade <kaustubhwade97@gmail.com>
2020-11-05 16:38:24 +0530

    Added Comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// function to open mobile navigation
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
 }
 
+// function to open mobile navigation
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
